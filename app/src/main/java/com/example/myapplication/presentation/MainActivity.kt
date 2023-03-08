effb20f5c48b00ef3ce998165b0ca6f508cda8b1
blastka <svetuk505@mail.ru>
2023-03-08 20:34:03 +0300

    add NavigationStrategy. While click on button does the wrong things.

--- app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.myapplication.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.myapplication.R
+import com.example.myapplication.animal.presentation.AnimalFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ShowFragment {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            NavigationStrategy.Replace(AnimalFragment()).navigate(supportFragmentManager, R.id.container)
+        }
+    }
+
+    override fun show(fragment: Fragment) {
+        NavigationStrategy.Add(fragment).navigate(supportFragmentManager, R.id.container)
+    }
+}
+
+interface ShowFragment{
+    fun show(fragment: Fragment)
+
+    class Empty(): ShowFragment {
+        override fun show(fragment: Fragment) {
+            return Unit
+        }
     }
 }
