d2bb65d7caa1a6fc93b7506360e9a49a2cc06162
blastka <svetuk505@mail.ru>
2023-04-12 12:55:18 +0300

    ViewModel

---
+++ app/src/main/java/com/example/myapplication/presentation/ManageResources.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication.presentation
+
+import android.content.Context
+import androidx.annotation.StringRes
+
+interface ManageResources {
+
+    fun string(@StringRes id: Int): String
+
+    class Base(private val context: Context): ManageResources{
+        override fun string(id: Int): String {
+            return context.getString(id)
+        }
+
+    }
+
+}
